10bb53e5e0297fb114cf7eb7b0314e1b92b13c99
Hoang Hung <hth321@gmail.com>
2021-09-08 23:33:34 +0700

    fixed lint issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
 import { store } from '../src/redux/store'
- 
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <Provider store={store}>
@@ -9,5 +9,5 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     </Provider>
   )
 }
- 
-export default  MyApp 
+
+export default MyApp
